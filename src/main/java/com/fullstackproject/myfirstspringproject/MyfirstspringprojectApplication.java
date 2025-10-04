4b9f60a2f4d0259c7620ac4417cceade1c6f0d65
mdnadeemanwar <anwarnadeem2010@gmail.com>
2025-10-05 02:04:31 +0530

    this is my first commit

--- src/main/java/com/fullstackproject/myfirstspringproject/MyfirstspringprojectApplication.java
+++ src/main/java/com/fullstackproject/myfirstspringproject/MyfirstspringprojectApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyfirstspringprojectApplication {
 
 	public static void main(String[] args) {
-
         SpringApplication.run(MyfirstspringprojectApplication.class, args);
 	}
 
