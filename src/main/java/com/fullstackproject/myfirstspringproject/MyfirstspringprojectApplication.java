681d078e1aaf106a3477228e4cbf3a3ee0b252f0
Nadeem-Macbook Pro <justmac@JustMacs-MacBook-Pro.local>
2025-10-05 01:36:50 +0530

    first spring boot project commit

---
+++ src/main/java/com/fullstackproject/myfirstspringproject/MyfirstspringprojectApplication.java
@@ -0,0 +1,14 @@
+package com.fullstackproject.myfirstspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstspringprojectApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(MyfirstspringprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullstackproject/myfirstspringproject/MyfirstspringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackproject.myfirstspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstspringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
