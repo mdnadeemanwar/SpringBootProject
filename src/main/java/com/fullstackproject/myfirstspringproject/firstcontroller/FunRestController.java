770d75a099a0157555d4811d676c023722c0cd0f
mdnadeemanwar <anwarnadeem2010@gmail.com>
2025-10-05 20:30:40 +0530

    Create first endpoint

---
+++ src/main/java/com/fullstackproject/myfirstspringproject/firstcontroller/FunRestController.java
@@ -0,0 +1,14 @@
+package com.fullstackproject.myfirstspringproject.firstcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello Nadeem";
+    }
+
+
+}
